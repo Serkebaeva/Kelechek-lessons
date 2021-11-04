3baa41f5561662d76473433dc889a0ee9d5b4971
AizadaSerkebaeva <aizada.serkebaeva.pls@gmail.com>
2021-11-04 14:36:05 +0600

    lesson-5 JS, Null, Undefined, Boolean and If or else

---
+++ Lesson-5/js.js
@@ -0,0 +1,43 @@
+//1. В переменную **`lang`** нужно получить данные от пользователя если пользователь введет “**`ru`**” нужно приветствовать его на русском если “**`ky`**” на кыргызском если будет другое значение нужно вывести сообщение “Я не знаю такого языка”
+
+/*let lang = prompt('Which Language do you choose? Type in "ru", "en" or "ky"');
+
+if (lang == "ru") {
+  console.log("Здравствуйте, гость! Добро пожаловать!");
+  document.write("Здравствуйте, гость! Добро пожаловать!");
+} else if (lang == "en") {
+  console.log("Hello, Dear Guest! Your are Welcome!");
+  document.write("Hello, Dear Guest! Your are Welcome!");
+} else if (lang == "ky") {
+  console.log("Саламатсызбы, урматтуу конок! Кош келдиниз!");
+  document.write("Саламатсызбы, урматтуу конок! Кош келдиниз!");
+} else {
+  console.log("Я не знаю такого языка");
+  document.write("Я не знаю такого языка");
+}
+*/
+// 2. Дана строка, состоящая из символов, например, '**`abcde`**'. Проверьте, что первым символом этой строки является буква '**`a`**'. Если это так - выведите '**`да`**', в противном случае выведите '**`нет`**'.
+
+/*let string = "**`abcde`**";
+
+if (string[0] === "a") {
+  console.log("yes!");
+  document.write("yes!");
+} else {
+  console.log("No!");
+  document.write("No!");
+}*/
+
+//3. Дано число с цифрами, например, **`12345`**. Проверьте, что первая цифра числа 1, 2 или 3. Если это так - выведите '**`да`**', в противном случае выведите '**`нет`**'.
+
+/*let number = `12345`;
+console.log(typeof number);
+
+if (number[0] == 1 || number[0] == 2 || number[0] == 3) {
+  console.log("yes!");
+  document.write("yes!");
+} else {
+  console.log("No!");
+  document.write("No!");
+}
+*/
